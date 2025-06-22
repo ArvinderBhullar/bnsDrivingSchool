9140ff30bec9925f750e517ede3dfe90d8c62a22
ArvinderBhullar <114533843+ArvinderBhullar@users.noreply.github.com>
2025-06-22 12:31:54 -0600

    Publish site to aws

--- driving-school-website/next.config.ts
+++ driving-school-website/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   output: 'export',
-  basePath: "/bnsDrivingSchool",
+  // basePath: "/bnsDrivingSchool",
   images: {
     unoptimized: true,
   },
